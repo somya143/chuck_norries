f4ff7b96e9d124cb95fcdabb1fcfca522a34e3c8
somya143 <maaheersj@gmail.com>
2023-07-06 00:28:10 +0530

    added cards to dom

--- src/Pages/ChuckNorris.jsx
+++ src/Pages/ChuckNorris.jsx
@@ -1,12 +1,31 @@
-import { Box, Heading } from '@chakra-ui/react'
-import React from 'react';
+import { Box, Heading, Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react';
 import "./chunckNorris.css";
+import { useSelector, useDispatch } from 'react-redux';
+import { getDataByCategory } from '../Redux/chuckNorrisData/action';
 
 const ChuckNorris = () => {
+  const {data , isLoading , isError} = useSelector((store) => store.chuckData)
+  const [chData , setChData] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+   dispatch(getDataByCategory())
+  }, [dispatch])
   return (
      <Box w={"100%"} h={"100vh"} bgGradient='linear(to-l,#222222, rgba(100,180,349,28.5))'>
       <Box className='bounceBox'>
       <Heading color={"#22C55E"} className='bounce'>Chuck Norries</Heading>
+      </Box>
+      <Box mt={"30px"} className='displayBox'>
+        {
+          data && data.map((el) => {
+            return <Box className='card'>
+              <Heading className='headingCard'>{el}</Heading>
+              <Text>{`Unlimited jokes on ${el}`}</Text>
+            </Box>
+          })
+        }
       </Box>
      </Box>
   )
